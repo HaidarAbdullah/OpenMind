55e70a97b940ec49ecb710d8c38f142f4cc8abe8
Haidar Abdullah <haidar66pen11@gmail.com>
2023-04-25 12:45:36 +0300

    update comment model attributes

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'id',
+        'content',
+        'user_id',          //foriegn key
+        'article_id',          //foriegn key
+    ];
 }
