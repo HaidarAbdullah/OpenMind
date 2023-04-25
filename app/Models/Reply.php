6bea3fb18c70ecfc62b1e40c9b6076ac88921e96
Haidar Abdullah <haidar66pen11@gmail.com>
2023-04-25 12:48:32 +0300

    update reply model attributes

--- app/Models/Reply.php
+++ app/Models/Reply.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Reply extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'id',
+        'content',
+        'user_id',          //foriegn key
+        'comment_id',          //foriegn key
+    ];
 }
