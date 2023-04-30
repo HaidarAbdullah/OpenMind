4b3d98a10f092fd44bf7e04111f05cfa7ba6f5ed
Haidar Abdullah <haidar66pen11@gmail.com>
2023-04-30 12:40:53 +0300

    add article model relations

--- app/Models/Aritcle.php
+++ app/Models/Aritcle.php
@@ -16,6 +16,25 @@ class Aritcle extends Model
         'user_id'          //foriegn key
     ];
 
+    ####################### Relations Begin #######################
+    public function user(){
+        return $this -> belongsTo('App\Models\User','User_id');
+    }
+    
+    public function comment(){
+        return $this -> hasMany('App\Models\Comment','article_id');
+    }
+
+    public function tag(){
+        return $this -> hasMany('App\Models\Tag','article_id');
+    }
+
+    public function reference(){
+        return $this -> hasMany('App\Models\Reference','article_id');
+    }
+
+    #######################  Relations End  #######################
+
 }
 
 
