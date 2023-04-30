45c13ed888ec4d50a9b6f7a618db7a2b37393844
Haidar Abdullah <haidar66pen11@gmail.com>
2023-04-30 13:08:54 +0300

    add sponsor model relations

--- app/Models/Sponsor.php
+++ app/Models/Sponsor.php
@@ -13,4 +13,16 @@ class Sponsor extends Model
         'user_id',          //foriegn key
         'brand_id',          //foriegn key
     ];
+    
+    ####################### Relations Begin #######################
+    public function user(){
+        return $this -> belongsTo('App\Models\User','User_id');
+    }
+    
+    public function brand(){
+        return $this -> belongsTo('App\Models\Brand','brand_id');
+    }
+    
+    #######################  Relations End  #######################
+
 }
